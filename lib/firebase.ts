2c943a1b64a521c3b18ce82322fe5b8e84d15c47
Abhijna <abhijnashetty2005@gmail.com>
2026-02-27 12:47:24 +0530

    connected real firebase project

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxxxxxxxxxxxx",
-  authDomain: "bloodbank-123.firebaseapp.com",
-  projectId: "bloodbank-123",
-  storageBucket: "bloodbank-123.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcd1234"
+  apiKey: "AIzaSyC62zxpffQJjiMhz8PF_Kd7rHV-gybihZE",
+  authDomain: "blood-bank-system-c0e6a.firebaseapp.com",
+  projectId: "blood-bank-system-c0e6a",
+  storageBucket: "blood-bank-system-c0e6a.firebasestorage.app",
+  messagingSenderId: "187482739262",
+  appId: "1:187482739262:web:fc30e96efaa6d59e8e376f"
 };
 
 const app = initializeApp(firebaseConfig);
