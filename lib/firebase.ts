40cdeda21c2610bf5a5f1dfdf65caa7e6d46be2e
Abhijna <abhijnashetty2005@gmail.com>
2026-02-27 11:50:03 +0530

    fixed firebase api key

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_KEY",
-  authDomain: "YOUR_REAL_DOMAIN",
-  projectId: "YOUR_REAL_ID",
-  storageBucket: "YOUR_REAL_BUCKET",
-  messagingSenderId: "YOUR_REAL_SENDER_ID",
-  appId: "YOUR_REAL_APP_ID",
+  apiKey: "PASTE_YOUR_REAL_API_KEY",
+  authDomain: "PASTE_YOUR_AUTH_DOMAIN",
+  projectId: "PASTE_YOUR_PROJECT_ID",
+  storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
